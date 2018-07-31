9f50bd7753f37d1334c9c34a9159d145a6dcd655
aguamenti <a@zalee.me>
2018-07-31 16:27:29 -0700

    woking on part 1 phase 5

--- todos/bundle.js
+++ todos/bundle.js
@@ -215,6 +215,8 @@ var _utils2 = _interopRequireDefault(_utils);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -229,46 +231,56 @@ var TodoForm = function (_React$Component) {
 
     var _this = _possibleConstructorReturn(this, (TodoForm.__proto__ || Object.getPrototypeOf(TodoForm)).call(this, props));
 
-    _this.createHandler = _this.createHandler.bind(_this);
+    _this.state = { title: "", body: "", done: false };
+    _this.handleSubmit = _this.handleSubmit.bind(_this);
+    _this.updateVal = _this.updateVal.bind(_this);
     return _this;
   }
 
   _createClass(TodoForm, [{
-    key: 'createHandler',
-    value: function createHandler(event) {
+    key: 'handleSubmit',
+    value: function handleSubmit(event) {
       event.preventDefault();
-      var id = (0, _utils2.default)();
-      var title = document.querySelector("input.input").value;
-      var body = document.querySelector("textarea.input").value;
-      var todo = { id: id, title: title, body: body };
+      var todo = Object.assign({}, this.state, { id: (0, _utils2.default)() });
       this.props.receiveTodo(todo);
+      this.setState({ title: '', body: '' });
+    }
+  }, {
+    key: 'updateVal',
+    value: function updateVal(el) {
+      var _this2 = this;
+
+      return function (e) {
+        return _this2.setState(_defineProperty({}, el, e.target.value));
+      };
     }
   }, {
     key: 'render',
     value: function render() {
-      var createHandler = this.createHandler;
+      var handleSubmit = this.handleSubmit,
+          updateVal = this.updateVal;
 
       return _react2.default.createElement(
         'div',
         null,
         _react2.default.createElement(
           'form',
-          null,
+          { onSubmit: handleSubmit },
           _react2.default.createElement(
             'label',
             null,
             'Title:',
-            _react2.default.createElement('input', { className: 'input', placeholder: 'buy milk', required: true })
+            _react2.default.createElement('input', { onChange: updateVal('title'), id: 'title', placeholder: 'buy milk', value: this.state.title, required: true })
           ),
           _react2.default.createElement(
             'label',
             null,
             'Body:',
-            _react2.default.createElement('textarea', { className: 'input', cols: '20', rows: '5', required: true })
+            _react2.default.createElement('textarea', { onChange: updateVal('body'), id: 'body', cols: '20', rows: '5', value: this.state.body, required: true })
           ),
           _react2.default.createElement(
             'button',
-            { onClick: createHandler, className: 'create-button' },
+            { className: 'create-button' },
             'Create Todo!'
           )
         )
--- todos/frontend/components/todos/todo_list_item.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TodoListItem = ({todo}) => (
-  <li>{todo.title}</li>
-);
-
-export default TodoListItem;
